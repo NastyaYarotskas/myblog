a9d083d6c4c802800b42ed2e2894906af55ac6ab
Nastya Yarotskas <nastyayarotskas@gmail.com>
2025-02-14 06:40:16 +0300

    Support tags save and update (#18)

--- src/main/java/ru/yandex/practicum/mapper/PostMapper.java
+++ src/main/java/ru/yandex/practicum/mapper/PostMapper.java
@@ -1,11 +1,15 @@
 package ru.yandex.practicum.mapper;
 
 import ru.yandex.practicum.model.Post;
+import ru.yandex.practicum.model.Tag;
 import ru.yandex.practicum.request.PostRequest;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class PostMapper {
 
@@ -13,13 +17,20 @@ public class PostMapper {
         byte[] bytes = request.getImage().getBytes();
         String imageBase64 = Base64.getEncoder().encodeToString(bytes);
 
+        Optional.ofNullable(request.getTags()).orElse("");
+
+        Set<Tag> tags = Arrays.stream(Optional.ofNullable(request.getTags()).orElse("").split(","))
+                .toList().stream()
+                .map(tag -> new Tag(0L, tag.trim()))
+                .collect(Collectors.toSet());
+
         return new Post(
                 0L,
                 request.getTitle(),
                 imageBase64,
                 request.getContent(),
                 0,
-                request.getTags(),
+                tags,
                 Set.of()
         );
     }
--- src/test/java/ru/yandex/practicum/it/JdbcNativePostRepositoryTest.java
+++ src/test/java/ru/yandex/practicum/it/JdbcNativePostRepositoryTest.java
@@ -8,8 +8,11 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import ru.yandex.practicum.configuration.DataSourceConfiguration;
 import ru.yandex.practicum.model.Page;
 import ru.yandex.practicum.model.Post;
+import ru.yandex.practicum.model.Tag;
 import ru.yandex.practicum.repository.JdbcNativePostRepository;
 import ru.yandex.practicum.repository.PostRepository;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -45,5 +48,15 @@ public class JdbcNativePostRepositoryTest {
         assertNotNull(posts);
         assertEquals(1, posts.getCollection().size());
         assertEquals(1, posts.getTotalPages(1));
+    }
+
+    @Test
+    void save_shouldReturnAllPostWithRequiredTags() {
+        postRepository.save(new Post(1L, "test", "test",
+                "test", 1, Set.of(new Tag(1L, "tag")), Set.of()));
+
+        Page<Post> posts = postRepository.findAll(0, 1);
+
+        assertNotNull(posts);
     }
 }
